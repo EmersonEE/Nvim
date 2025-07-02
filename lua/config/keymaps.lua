01090f380d7cfd4cb1fac3667b5533caa62f9359
EmersonEE <perezemerson85@gmail.com>
2025-07-01 21:17:25 -0600

    Agranddo Atajos para PlatformIO

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+-- -- Keymap para insertar texto PlatformIO config
+vim.keymap.set("n", "<leader>pi", function()
+  local lines = {
+    "monitor_speed = 115200",
+    "build_flags = ",
+    "  -Iinclude",
+    ""
+  }
+  vim.api.nvim_put(lines, "l", true, true)
+end, { desc = "Insertar config PIO" })
+
+-- Keymap para insertar texto PlatformIO config
+vim.keymap.set("n", "<leader>po", function()
+  local lines = {
+    "#include <Arduino.h>",
+    "void setup() {",
+    "}",
+    "void loop(){",
+    "}",
+    ""
+  }
+  vim.api.nvim_put(lines, "l", true, true)
+end, { desc = "Insertar config PIO" })
